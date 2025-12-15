f597cabc82b5121a9ff1cc305928d6f6be81fe10
quan2407 <quan24072003@gmail.com>
2025-12-15 09:41:02 +0700

    generate jwt token

---
+++ src/main/java/com/example/mini_drive_storage/service/JWTService.java
@@ -0,0 +1,42 @@
+package com.example.mini_drive_storage.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Service
+public class JWTService {
+
+    private static final String SECRET_KEY =
+            "my-super-secret-key-my-super-secret-key-256bit";
+
+    public String generateToken(String email) {
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(email)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 30))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getKey() {
+        byte[] keyBytes = SECRET_KEY.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
+
